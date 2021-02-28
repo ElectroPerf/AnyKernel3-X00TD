8a86f03439ba96795f47cd5a686b6767ad8fb119
Curious-To-Learn <alokpravabarik@gmail.com>
2021-02-28 17:42:22 +0000

    Point out Android version

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -14,9 +14,9 @@ device.name1=X01BD
 device.name2=X01BDA
 device.name3=ASUS_X01BD
 device.name4=ASUS_X01BDA
-device.name5=ASUS_X01BD_1
+device.name5=
 supported.versions=
-supported.patchlevels=
+supported.patchlevels=11.0
 '; } # end properties
 
 # shell variables
